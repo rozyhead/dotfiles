17e85d4cb7d97fb7b7edc4052505c42a6d7d88a5
T.Miyajima <rozyhead@gmail.com>
2014-07-08 02:23:27 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname "$0")
 for dotfile in .?*; do
-  if [ $dotfile != '..' ] && [ $dotfile != '.git' ]; then
-    ln -Fis "$PWD/$dotfile" $HOME/$dotfile
+  if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitmodules' ]; then
+    ln -Fis "$PWD/$dotfile" $HOME
   fi
 done
