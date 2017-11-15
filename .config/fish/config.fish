b2299b737b59a8ab3a7fe40fd9b8edec084b586c
Takeshi Miyajima <rozyhead@gmail.com>
2017-11-16 00:19:02 +0900

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
 balias g 'git'
-balias gst 'git st'
-balias gcm 'git cm'
+balias gst 'git status -sb; and git stash list'
+balias gcm 'git commit -v'
+balias gch 'git cherry-pick -v'
+balias glgg 'git lgg'
+balias glg 'git lgg | head'
+balias gfs 'git flow feature start'
+balias gff 'git flow feature finish'
+
+set -U fish_user_paths $fish_user_paths $HOME/.nodebrew/current/bin
